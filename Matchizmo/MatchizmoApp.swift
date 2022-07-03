7d19b8a5ad89dd483fde5cbdd46a8a49c7d330d8
Rams Tanis <rams3377@gmail.com>
2022-07-03 08:07:51 -0500

    Initial Commit

---
+++ Matchizmo/MatchizmoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchizmoApp.swift
+//  Matchizmo
+//
+//  Created by Rams Tanis on 7/3/22.
+//
+
+import SwiftUI
+
+@main
+struct MatchizmoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
