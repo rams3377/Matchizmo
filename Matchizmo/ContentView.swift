2933151c7f27ce6e1028e2c2016c0f1af512ea66
Rams Tanis <rams3377@gmail.com>
2022-07-03 10:49:31 -0500

    Simple Card - end of CS193P lesson 1

--- Matchizmo/ContentView.swift
+++ Matchizmo/ContentView.swift
@@ -9,16 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            RoundedRectangle(cornerRadius: 20.0)
+                .stroke(lineWidth: 3)
+            Text("♠️")
         }
+        .padding(.horizontal)
+        .foregroundColor(.red)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct ContentView_Previews: PreviewProvider  {
     static var previews: some View {
         ContentView()
     }
