75894f273d04876141b096664fdcd415391d38f2
Rams Tanis <rams3377@gmail.com>
2022-07-06 20:19:05 -0500

    Renamed and refactored.

--- Matchizmo/EmojiMemoryGameView.swift
+++ Matchizmo/EmojiMemoryGameView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  EmojiMemoryGameView.swift
 //  Matchizmo
 //
 //  Created by Rams Tanis on 7/3/22.
@@ -7,17 +7,17 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @ObservedObject var viewModel: EmojiMemoryGame
+struct EmojiMemoryGameView: View {
+    @ObservedObject var game: EmojiMemoryGame
     
     var body: some View {
         ScrollView(.vertical) {
             LazyVGrid(columns: [GridItem(.adaptive(minimum: 70))]) {
-                ForEach(viewModel.cards) { card in
+                ForEach(game.cards) { card in
                     CardView(card: card)
                         .aspectRatio(2/3, contentMode: .fit)
                         .onTapGesture {
-                            viewModel.choose(card)
+                            game.choose(card)
                         }
                 }
             }
@@ -31,6 +31,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider  {
     static var previews: some View {
-        ContentView(viewModel: EmojiMemoryGame())
+        EmojiMemoryGameView(game: EmojiMemoryGame())
     }
 }
--- Matchizmo/MatchizmoApp.swift
+++ Matchizmo/MatchizmoApp.swift
@@ -13,7 +13,7 @@ struct MatchizmoApp: App {
     
     var body: some Scene {
         WindowGroup {
-            ContentView(viewModel: game)
+            EmojiMemoryGameView(game: game)
         }
     }
 }
