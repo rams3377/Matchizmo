ba72de4794ac957b8ba8fc2337f03e82045e8c74
Rams Tanis <rams3377@gmail.com>
2022-07-08 18:11:04 -0500

    Update card font to be dynamic.

--- Matchizmo/CardView.swift
+++ Matchizmo/CardView.swift
@@ -11,20 +11,34 @@ struct CardView: View {
     var card: EmojiMemoryGame.Card
     
     var body: some View {
-        ZStack {
-            let shape = RoundedRectangle(cornerRadius: 20.0)
-            if card.isFaceUp {
-                shape.fill().foregroundColor(.white)
-                shape.strokeBorder(lineWidth: 3)
-                Text(card.content).font(.largeTitle)
-            } else if card.isMatched {
-                shape.opacity(0)
-            } else {
-                shape.fill()
+        GeometryReader { geometry in
+            ZStack {
+                let shape = RoundedRectangle(cornerRadius: 20.0)
+                if card.isFaceUp {
+                    shape.fill().foregroundColor(.white)
+                    shape.strokeBorder(lineWidth: 3)
+                    Text(card.content).font(font(in: geometry.size))
+                } else if card.isMatched {
+                    shape.opacity(0)
+                } else {
+                    shape.fill()
+                }
             }
         }
     }
+    
+    
+    private func font(in size: CGSize) -> Font {
+        Font.system(size: min(size.width, size.height) * 0.8)
+    }
 }
+
+private struct CardConstants {
+    static let cornerRadius: CGFloat = 20.0
+    static let lineWidth: CGFloat = 3.0
+    static let fontScale: CGFloat = 0.8
+}
+
 
 struct CardView_Previews: PreviewProvider {
     static var previews: some View {
