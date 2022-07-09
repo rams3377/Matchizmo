ae1c64f18e6ded7503624f0af1765947a4a941c6
Rams Tanis <rams3377@gmail.com>
2022-07-08 20:57:45 -0500

    Sure that all space is taken up and view is pushed to the top.

--- Matchizmo/AspectVGrid.swift
+++ Matchizmo/AspectVGrid.swift
@@ -14,11 +14,14 @@ struct AspectVGrid<Item, ItemView>: View where ItemView: View, Item: Identifiabl
     
     var body: some View {
         GeometryReader { geometry in
-            let width: CGFloat = withThatFits(itemCount: items.count, in: geometry.size, aspectRatio: 2/3)
-            LazyVGrid(columns: [adaptiveGridItem(width: width)], spacing: 0) {
-                ForEach(items) { item in
-                    content(item).aspectRatio(aspectRatio, contentMode: .fit)
+            VStack {
+                let width: CGFloat = withThatFits(itemCount: items.count, in: geometry.size, aspectRatio: 2/3)
+                LazyVGrid(columns: [adaptiveGridItem(width: width)], spacing: 0) {
+                    ForEach(items) { item in
+                        content(item).aspectRatio(aspectRatio, contentMode: .fit)
+                    }
                 }
+                Spacer()
             }
         }
     }
